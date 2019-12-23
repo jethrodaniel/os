ca47404e3b06b199de97376ddb4a27baf33af729
Mark Delk <jethrodaniel@gmail.com>
2019-12-23 00:01:54 -0600

    tmp

--- print_hex.asm
+++ print_hex.asm
@@ -4,6 +4,10 @@
 
 ; subroutine to print a hex number.
 ;
+; adapted from:
+; - https://stackoverflow.com/a/27686875/7132678
+; - https://config9.com/linux/how-to-print-a-number-in-assembly-nasm/
+;
 ; ```
 ; dx = the hexadecimal value to print
 ; ```
@@ -15,12 +19,41 @@
 ; call print_hex
 ; ```
 ;
+; this works like so:
+;
+; - the hex digit to print is in dx
+; - we set si to have the current digit in the global string
+;  - set bx to matc
+
+;
+; to print the first hex digit, we need to isolate the leftmost 4 bits
+; of the input's 2 bytes.
+;
+;   0xa31f = 0xf * 16^0 + 0x1 * 16^1 + 3 * 16^2 + 0xa * 16^3
+;
+; first digit:
+;
+;  - get that 'a' at the end of a string
+;
+;  0xa31f >> 3 => 0x000a
+;
+; then we & it by 0xf to isolate it from the rest
+;
+; to get the ASCII code of this digit, we need to convert it to its
+; ASCII value.
+;
+; if it's <= 9, then we can just add `0`'s ASCII (0x30), else we need to
+; add `a`'s ASCII (0x39).
 ;
 print_hex:
         ; push all registers onto the stack
         pusha
 
         ; start with the largest digit
+        ;
+        ; 0x 0 000
+        ;   | |
+        ;
         mov si, HEX_OUT + 2
 
 next_character:
@@ -28,15 +61,25 @@ next_character:
         mov bx, dx
 
         ; 0x1fb6
-        ; 000000000001fb6
-        ;       &
-        ; 0000000ff000000
+        ; 0001111110110110
         ;
-        ; -> 4
-        ; 000000000000000
+        ;    &
+        ;
+        ; 0xf000
+        ; 1111000000000000
+        ;
+        ; => 1000000000000 (0x1000)
         and bx, 0xf000
+
+        ; => 0000000100000000 (0x100)
         shr bx, 4
+
+        ; Add a '0' to this string.
+        ; 0x30, '0'
         add bh, 0x30
+
+        ; 0x39
+        ; 111001
         cmp bh, 0x39
         jg add_7
 
