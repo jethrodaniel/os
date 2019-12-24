f45a73854ac1abf1bc4cd3b865d8c52a21d664f3
Mark Delk <jethrodaniel@gmail.com>
2019-12-24 00:58:27 -0600

    tmp

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,4 +5,9 @@ int main(int argc, char** argv)
         char* video_memory = (char *)0xb8000;
 
         *video_memory = 'X';
+
+        print("yo!!!!!!!!!!!1!");
+
+        //int offset = get_screen_offset(3, 4);
+        //offset += '0';
 }
