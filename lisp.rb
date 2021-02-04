2bf191567f308827a04048d220feaae4339c287c
Mark Delk <jethrodaniel@gmail.com>
2021-02-04 12:41:57 -0600

    lisp rb prototype, start forming ops

--- lisp.rb
+++ lisp.rb
@@ -1,31 +1,49 @@
 require 'io/console'
 
-prompt = "> "
+prompt = "? "
+result_prompt = "=> "
 
 puts "[lisp] Started. `q` to exit"
 
 print prompt
 
-loop do
-  c = $stdin.noecho(&:getch).ord
+got_left_paren = false
+got_right_paren = false
+read_char = false
+operator = ''
+operands = []
 
-  if c == 113 # 'q'
+loop do
+  # Get one character at a time
+  char = $stdin.noecho(&:getch).ord
+
+  # Echo character when typed
+  print char.chr
+
+  # Exit on `q`.
+  #
+  # todo: rm this, replace with `(quit)`
+  if char == 113 # q
     print "\n"
     exit
   end
 
-  print c.chr
-  # print c
-
+  # Evaluate input on newline.
+  #
   # TODO: continue to next line if unclosed parens
-  if c == 13 # "\r"
-    print prompt
+  if char == 13 # \r
+    if operator == ''
+      print prompt
+    else
+      print result_prompt
+      print "op: #{operator}"
+    end
     print "\n"
     print prompt
     redo
   end
 
-
+  operator += char.chr
 end
 
 puts "[lisp] Exited."
